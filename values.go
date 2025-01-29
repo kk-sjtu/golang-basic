801743ea9cf17b674f9d63b3dd9c8f96328368f2
Your Name <you@example.com>
2025-01-30 01:18:47 +0800

    first commit

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("kk"+"fighting!")
+
+	fmt.Println("kk is No.",1)
+	fmt.Println("7.0/3.0 = ",7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
