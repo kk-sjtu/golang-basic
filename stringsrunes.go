9e6683153cce16b63b64c8e9bc9d0b4d6d45fa2c
Your Name <kkipaper@163.com>
2025-02-02 12:34:13 +0800

    string

---
+++ stringsrunes.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main(){
+	const s = "abc你好aaa"
+
+	fmt.Println("Len:",len(s))
+	for i := 0;i<len(s);i++{
+		fmt.Println("%x ",s[i])
+	}
+	fmt.Println()
+
+	fmt.Println("Rune count:",utf8.RuneCountInString(s))
+
+	for idx,runeValue := range s{
+		fmt.Println("%#U starts at %d\n",runeValue,idx)
+	}
+
+	fmt.Println("\nUsing DecodeRuneInString")
+
+	for i,w := 0,0;i<len(s);i += w{
+		runeValue,width := utf8.DecodeRuneInString(s[i:])
+		fmt.Println("%#U starts at %d\n",runeValue,i)
+		w = width
+		fmt.Println("width:",w)
+
+		examineRune(runeValue)
+	}
+}
+
+func examineRune(r rune){
+	if r == 'a'{
+		fmt.Println("found tee")
+	}else if r == '你' {
+		fmt.Println("found 你")}
+}
