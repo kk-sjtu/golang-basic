de1990dd11cc2dd8c781ad1364ecb410880efe1c
Your Name <kkipaper@163.com>
2025-02-05 00:42:24 +0800

    链表删除节点

--- go数据结构和算法/数组和链表/链表.go
+++ go数据结构和算法/数组和链表/链表.go
@@ -23,7 +23,16 @@ n2.Next = n3
 n3.Next = n4
 
 func insertNode(n0 *ListNode,P *ListNode){
-	n1 := n0.Next
+	n1 := n0.Next // 因为原先n0后面是有节点的，所以需要用n1来保存一下
 	P.next = n1
+	n0.Next = P
+}
 
+func removeItem(n0 ListNode){
+	if n0.Next == nil{
+		return
+	}
+	P := n0.Next
+	n1 := P.Next
+	n0.Next = n1
 }
